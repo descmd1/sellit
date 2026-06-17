f9f19404e0c0dc667cae0611f8e50ed8cbe409de
Criscodesand <christopher@marketedgeadvisory.com>
2026-06-17 15:37:44 +0100

    added route

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://sellit-6nyn.onrender.com/api',
   withCredentials: true,
 });
 
